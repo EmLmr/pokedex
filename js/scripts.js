535c9a7e847cc740b1d5373e28d31801454d96a0
EmLmr <emeline.coding@gmail.com>
2021-06-28 13:45:18 +0200

    manually entered pokemon data

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,33 @@
-alert('Hello world');
-
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
+// Pokemon data to display in app.
+let pokemonList = [
+  {
+  name: 'Bulbasaur',
+  height: 2,
+  types: ['grass', 'poison'],
+  abilities: 'overgrow'
+  },
+  {
+    name: 'Urshifu',
+    height: 6,
+    types: ['fighting', 'dark'],
+    abilities: 'unseen fist'
+  },
+  {
+    name: 'Wimpod',
+    height: 1,
+    types: ['bug', 'water'],
+    abilities: 'wimp out'
+  },
+  {
+    name: 'Roselia',
+    height: 1,
+    types: ['grass', 'poison'],
+    abilities: ['poison point', 'natural cure']
+  },
+  {
+    name: 'Infernape',
+    height: 3,
+    types: ['fire', 'fighting'],
+    abilities: 'flame'
+  }
+];
