42b3a9e3923a2fcf440803de159b997298f6ad0a
EmelineLmr <emeline.coding@gmail.com>
2021-07-02 14:34:10 +0200

    updated scripts.js with IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,51 @@
 // Pokemon data to display in app.
-const pokemonList = [{
-    name: 'Bulbasaur',
-    height: 2,
-    types: ['grass', 'poison'],
-    abilities: 'overgrow'
-  },
-  {
-    name: 'Urshifu',
-    height: 6,
-    types: ['fighting', 'dark'],
-    abilities: 'unseen fist'
-  },
-  {
-    name: 'Wimpod',
-    height: 1,
-    types: ['bug', 'water'],
-    abilities: 'wimp out'
-  },
-  {
-    name: 'Roselia',
-    height: 1,
-    types: ['grass', 'poison'],
-    abilities: ['poison point', 'natural cure']
-  },
-  {
-    name: 'Infernape',
-    height: 3,
-    types: ['fire', 'fighting'],
-    abilities: 'flame'
-  }
-];
+let pokemonRepository = (function () {
+  const pokemonList = [{
+      name: 'Bulbasaur',
+      height: 2,
+      types: ['grass', 'poison'],
+      abilities: 'overgrow'
+    },
+    {
+      name: 'Urshifu',
+      height: 6,
+      types: ['fighting', 'dark'],
+      abilities: 'unseen fist'
+    },
+    {
+      name: 'Wimpod',
+      height: 1,
+      types: ['bug', 'water'],
+      abilities: 'wimp out'
+    },
+    {
+      name: 'Roselia',
+      height: 1,
+      types: ['grass', 'poison'],
+      abilities: ['poison point', 'natural cure']
+    },
+    {
+      name: 'Infernape',
+      height: 3,
+      types: ['fire', 'fighting'],
+      abilities: 'flame'
+    }
+  ];
 
-pokemonList.forEach(function(pokemon) {
+    function getAll() {
+      return pokemonList;
+    }
+    function add(item) {
+      pokemonList.push(item);
+    }
+
+    return {
+      getAll: getAll,
+      add: add
+    };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
   // Display looped Pokémon on the DOM, with a line break.
   document.write('<br>' + '<h1>' + pokemon.name + '</h1>' + ` (height: ${pokemon.height}) `);
   //Conditional loop; checks if height is greater than 5.
