489ca2a5c9845175756f56c3ae446d68c0221d90
EmLmr <emeline.coding@gmail.com>
2021-07-06 20:54:35 +0200

    updated scripts.js
    - updated the add function with a conditional
    - streamlined the comments for cleaner code
    - corrected some keys' spelling in pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,82 +1,92 @@
 // Pokemon data to display in app.
 let pokemonRepository = (function() {
-      const pokemonList = [{
-          name: 'Bulbasaur',
-          height: 2,
-          types: ['grass', 'poison'],
-          abilities: 'overgrow'
-        },
-        {
-          name: 'Urshifu',
-          height: 6,
-          types: ['fighting', 'dark'],
-          abilities: 'unseen fist'
-        },
-        {
-          name: 'Wimpod',
-          height: 1,
-          types: ['bug', 'water'],
-          abilities: 'wimp out'
-        },
-        {
-          name: 'Roselia',
-          height: 1,
-          types: ['grass', 'poison'],
-          abilities: ['poison point', 'natural cure']
-        },
-        {
-          name: 'Infernape',
-          height: 3,
-          types: ['fire', 'fighting'],
-          abilities: 'flame'
-        },
-        {
-          name: 'Arcanine',
-          height: 6,
-          types: ['fire'],
-          abilities: ['Intimidate', 'Flash Fire']
-        }
-      ];
+  const pokemonList = [{
+      name: 'Bulbasaur',
+      height: 2,
+      type: ['grass', 'poison'],
+      ability: 'overgrow'
+    },
+    {
+      name: 'Urshifu',
+      height: 6,
+      type: ['fighting', 'dark'],
+      ability: 'unseen fist'
+    },
+    {
+      name: 'Wimpod',
+      height: 1,
+      type: ['bug', 'water'],
+      ability: 'wimp out'
+    },
+    {
+      name: 'Roselia',
+      height: 1,
+      type: ['grass', 'poison'],
+      ability: ['poison point', 'natural cure']
+    },
+    {
+      name: 'Infernape',
+      height: 3,
+      type: ['fire', 'fighting'],
+      ability: 'flame'
+    },
+    {
+      name: 'Arcanine',
+      height: 6,
+      type: ['fire'],
+      ability: ['Intimidate', 'Flash Fire']
+    }
+  ];
 
-      // returns an array of all the Pokemon in pokemonList
-      function getAll() {
-        return pokemonList;
-      }
+  // returns an array of all the Pokemon in pokemonList
+  function getAll() {
+    return pokemonList;
+  }
 
-      // adds new Pokemon to the pokemonList array
-      function add(pokemon) {
-        pokemonList.push(pokemon);
-      }
+  // adds new Pokemon to the pokemonList array with a conditional to make sure the correct type of data is entered.
+  function add(pokemon) {
+    if (
+      typeof pokemon === 'object' &&
+      'name' in pokemon &&
+      'height' in pokemon &&
+      'type' in pokemon &&
+      'ability' in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log('Incorrect Pokemon entry.');
+    }
+  }
 
-      // function that adds the list of Pokemon to the DOM, as buttons
-      function addListItem(pokemon) {
-        let pokemonList = document.querySelector('.pokemon-list'); // selects the HTML class "pokemon-list", which is an unordered list
+  // function that adds the list of Pokemon to the DOM, as buttons in an unordered list.
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
 
-        let listItem = document.createElement('li'); // creates a new list item
+    let listItem = document.createElement('li');
 
-        let button = document.createElement('button'); // creates a new button element
-        button.innerText = pokemon.name; // the button displays a pokemon name
-        button.classList.add('list-item'); // adds the class name "list-item" to the button
-        listItem.appendChild(button); // the button becomes a list item
-        pokemonList.appendChild(listItem); // each pokemon item in the list passes their corresponding value from the IIFE object
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('list-item');
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
 
-        button.addEventListener('click', function(){ // the showDetails() function will be executed when the user clicks on the Pokemon's button
-			       showDetails(pokemon);
-		    })
-      };
+    button.addEventListener('click', function() { // the showDetails() function will be executed when the user clicks on the Pokemon's button
+      showDetails(pokemon);
+    })
+  };
 
-      // for now, clicking on the Pokemon's button will print its name in the console
-      function showDetails(pokemon) {
-        console.log(pokemon.name);
-      };
+  // for now, clicking on the Pokemon's button will print its name in the console
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  };
 
-        return {
-          getAll: getAll,
-          add: add,
-          addListItem: addListItem,
-        };
-      })();
+  return {
+    getAll: getAll,
+    add: add,
+    addListItem: addListItem,
+  };
+})();
 
-    pokemonRepository.getAll().forEach(function(pokemon) {
-      pokemonRepository.addListItem(pokemon); // each pokemon is added to the pokemon list
-    });
+pokemonRepository.getAll().forEach(function(pokemon) {
+  pokemonRepository.addListItem(pokemon); // each pokemon is added to the pokemon list
+});
